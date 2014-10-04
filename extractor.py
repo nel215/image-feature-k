acd2c82dd8aed3c62b1e548e806b591578895508
nel215 <otomo.yuhei@gmail.com>
2014-10-04 20:25:48 +0900

    feature extractの高速化

--- extractor.py
+++ extractor.py
@@ -11,24 +11,30 @@ def extract_features_from_img(img, kmeans, patch_size=5):
     k = kmeans.get_params()['n_clusters']
     h,w,c = img.shape
     offset = patch_size/2
-    feature = [0 for _ in xrange(4*k)]
+    vectors = []
+    indices = []
     for y in xrange(offset, h-offset):
         for x in xrange(offset, w-offset):
             patch = cv2.getRectSubPix(img, (patch_size, patch_size), (x,y))
             vector = vectorizer.extract_vector_from_patch(patch)
-            dist = kmeans.transform(vector)
-            bin = dist.argmin()
-            min_dist = dist.min()
-            mean = dist.mean()
-            strength = max(0, mean - min_dist)
-            idx = (2*(2*y/h)+(2*x/w))*k + bin
-            feature[idx] += strength
+            vectors.append(vector)
+            idx = (2*(2*y/h)+(2*x/w))*k
+            indices.append(idx)
+
+    feature = [0 for _ in xrange(4*k)]
+    distances = kmeans.transform(vectors)
+    for dist, idx in zip(distances, indices):
+        bin = dist.argmin()
+        min_dist = dist.min()
+        mean = dist.mean()
+        strength = max(0, mean - min_dist)
+        feature[idx+bin] += strength
     feature = numpy.array(feature)
     feature /= feature.mean()
     feature /= feature.std()
     return feature
 
-f = open('./cluster-2.db', 'r')
+f = open('./cluster.db', 'r')
 kmeans = pickle.load(f)
 f.close()
 
@@ -37,15 +43,17 @@ base_dir = './101_ObjectCategories/'
 train_data = []
 
 categories = os.listdir(base_dir)
-for category in categories[:2]:
+for category in categories[:5]:
     img_dir = base_dir + category + '/'
     image_names = os.listdir(img_dir)
     for image_name in image_names[10:20]:
         print img_dir, image_name
         img = cv2.imread(img_dir + image_name)
         h,w,c = img.shape
-        small_img = cv2.resize(img, (w/2, h/2))
-        feature = extract_features_from_img(small_img, kmeans)
+        resize_ratio = 64.1/max(h,w)
+        resize_size = (int(w*resize_ratio), int(h*resize_ratio))
+        small_img = cv2.resize(img, resize_size)
+        feature = extract_features_from_img(small_img, kmeans, 9)
         train_data.append((feature, category))
 
 f = open('./train_data.db', 'w')
