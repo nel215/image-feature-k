cb9757cc06c2737fddefdf51045a5f8964ec24cb
nel215 <otomo.yuhei@gmail.com>
2014-10-04 15:14:41 +0900

    クラスタリングの実装

---
+++ clustering.py
@@ -0,0 +1,16 @@
+# utf-8
+
+import pickle
+import numpy
+import sklearn.cluster
+
+f = open('./features.db', 'r')
+vectors = pickle.load(f)
+f.close()
+
+print 'start clustering.'
+kmeans = sklearn.cluster.MiniBatchKMeans()
+kmeans.fit(numpy.array(vectors))
+
+f = open('./cluster.db', 'w')
+pickle.dump(kmeans, f)
