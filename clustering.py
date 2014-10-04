b1fe838c0ca8e8a934b69ff22cb803c4ded716c1
nel215 <otomo.yuhei@gmail.com>
2014-10-04 16:39:47 +0900

    clusterの数を増やしました

--- clustering.py
+++ clustering.py
@@ -1,17 +1,21 @@
 # utf-8
 
+import time
 import pickle
 import numpy
 import sklearn.cluster
 
-f = open('./features.db', 'r')
+f = open('./features-2.db', 'r')
 vectors = pickle.load(f)
 f.close()
 
 print 'start clustering.'
-kmeans = sklearn.cluster.MiniBatchKMeans()
+start_time = time.clock()
+kmeans = sklearn.cluster.MiniBatchKMeans(n_clusters=100)
 kmeans.fit(numpy.array(vectors))
+end_time = time.clock()
+print "elasped time:", end_time - start_time
 
-f = open('./cluster.db', 'w')
+f = open('./cluster-2.db', 'w')
 pickle.dump(kmeans, f)
 f.close()
